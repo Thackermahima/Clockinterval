38b139c4ad51e0febf4855c50ae69726202a0a87
Mahima <mahimathacker75@gmail.com>
2022-03-04 21:53:57 +0530

    Completed clockproject

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+function App() {
+const now  = new Date().toLocaleTimeString();
+const [time , setTime] = React.useState(now);
+setInterval(updateTime, 1000);
+function updateTime(){
+
+  const newTime = new Date().toLocaleTimeString();
+  setTime(newTime);
+}
+  return (
+    <div className='container'>
+    <h1>{time}</h1>
+    <button onClick={updateTime}>Get time</button>
+    </div>
+  );
+}
+
+export default App;
